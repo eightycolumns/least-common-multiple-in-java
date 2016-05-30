348cb4a79d8b5448e411f2d01b5a5815cd158051
Drew Keller <drew@eightycolumns.com>
2016-05-30 18:50:53 -0400

    Reword several tests

--- LeastCommonMultipleTest.java
+++ LeastCommonMultipleTest.java
@@ -27,32 +27,32 @@ public class LeastCommonMultipleTest {
   }
 
   @Test
+  public void theLeastCommonMultipleOf2And2Is2() {
+    assertEquals(2, LeastCommonMultiple.ofIntegers(2, 2));
+  }
+
+  @Test
+  public void theLeastCommonMultipleOf2And4Is4() {
+    assertEquals(4, LeastCommonMultiple.ofIntegers(2, 4));
+  }
+
+  @Test
   public void theLeastCommonMultipleOf2And3Is6() {
     assertEquals(6, LeastCommonMultiple.ofIntegers(2, 3));
   }
 
   @Test
+  public void theLeastCommonMultipleOfPositive2AndNegative3Is6() {
+    assertEquals(6, LeastCommonMultiple.ofIntegers(2, -3));
+  }
+
+  @Test
+  public void theLeastCommonMultipleOfNegative2AndPositive3Is6() {
+    assertEquals(6, LeastCommonMultiple.ofIntegers(-2, 3));
+  }
+
+  @Test
   public void theLeastCommonMultipleOf2And3And5Is30() {
     assertEquals(30, LeastCommonMultiple.ofIntegers(2, 3, 5));
-  }
-
-  @Test
-  public void theLeastCommonMultipleOfPositive7AndNegative11Is77() {
-    assertEquals(77, LeastCommonMultiple.ofIntegers(7, -11));
-  }
-
-  @Test
-  public void theLeastCommonMultipleOfNegative7AndPositive11Is77() {
-    assertEquals(77, LeastCommonMultiple.ofIntegers(-7, 11));
-  }
-
-  @Test
-  public void theLeastCommonMultipleOf10And10Is10() {
-    assertEquals(10, LeastCommonMultiple.ofIntegers(10, 10));
-  }
-
-  @Test
-  public void theLeastCommonMultipleOf10And100Is100() {
-    assertEquals(100, LeastCommonMultiple.ofIntegers(10, 100));
   }
 }
